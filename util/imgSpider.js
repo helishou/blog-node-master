e7c470777103056cf4f7a2019e36f4a9b63f4324
helishou <41136716+helishou@users.noreply.github.com>
2021-07-14 18:59:22 +0800

    111

--- util/imgSpider.js
+++ util/imgSpider.js
@@ -1,7 +1,7 @@
 /*
  * @Author       : helishou
  * @Date         : 2021-07-13 23:46:18
- * @LastEditTime : 2021-07-14 17:54:16
+ * @LastEditTime : 2021-07-14 18:59:16
  * @LastEditors  : helishou
  * @Description  :
  * @FilePath     : \util\imgSpider.js
@@ -31,6 +31,7 @@ function imgSpider(url, dest = "", timeout = 1000 * 3 * 60, retries = 2) {
   // console.log(url)
   let tempArr=url.split("/")
   dest = dest + tempArr[tempArr.length-1];
+  console.log('存在：',dest)
   let isRetry = false;
   let req = http.request(url, (res) => res.pipe(fs.createWriteStream(dest)));
   req.setTimeout(timeout, () => {
