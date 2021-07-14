9e1bdfac9c1b87f10cd3c55f96fb95f2e37ac1c8
helishou <41136716+helishou@users.noreply.github.com>
2021-07-14 14:52:24 +0800

    111

---
+++ util/img/index.js
@@ -0,0 +1,12 @@
+/*
+ * @Author       : helishou
+ * @Date         : 2021-07-14 14:50:50
+ * @LastEditTime : 2021-07-14 14:51:17
+ * @LastEditors  : helishou
+ * @Description  : 
+ * @FilePath     : \util\img\index.js
+ * 你用你的指尖,阻止我说再见,在bug完全失去之前
+ */
+
+const imgSpider = require('./../imgSpider')
+
--- util/imgSpider.js
+++ util/imgSpider.js
@@ -1,20 +1,22 @@
 /*
  * @Author       : helishou
  * @Date         : 2021-07-13 23:46:18
- * @LastEditTime : 2021-07-14 00:58:30
+ * @LastEditTime : 2021-07-14 14:50:22
  * @LastEditors  : helishou
  * @Description  :
- * @FilePath     : f:\桌面\front do\blog-node-master\blog-node-master\util\imgSpider.js
+ * @FilePath     : \util\imgSpider.js
  * 你用你的指尖,阻止我说再见,在bug完全失去之前
  */
 const fs = require("fs");
 const http = require("http");
+var webp = require("webp-converter");
 // const { url } = require('inspector')
 const userAgent =
   "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36";
 const cookie =
-  "__yjs_duid=1_e78758ba8eb8abdc4912b53e67d9eba71622563085537; yjs_js_security_passport=930696995000b6c7c36e98e1602f28fb4cbca4f2_1626193598_js";
+  "__yjs_duid=1_d2e90dd9423720235fb02211aa9751dd1626244512341; yjs_js_security_passport=4e1767e15d7e78523b259330da8b11ce52741d84_1626245048_js; td_cookie=2035685763";
 const host = "img.netbian.com";
+
 /**
  * 图片下载器
  * @param {string} url - 图片的网络地址
@@ -23,6 +25,9 @@ const host = "img.netbian.com";
  * @param {number} retries - 重试次数，默认重试 2 次
  */
 function imgSpider(url, dest = "", timeout = 1000 * 3 * 60, retries = 2) {
+  // imageProcessing
+  // .webp()
+  // .pipe(someWritableStream);
   dest = dest + url.split("file")[1].split("/")[3];
   let isRetry = false;
   let req = http.request(url, (res) => res.pipe(fs.createWriteStream(dest)));
@@ -31,9 +36,11 @@ function imgSpider(url, dest = "", timeout = 1000 * 3 * 60, retries = 2) {
     isRetry = true;
   });
   req.setHeader("User-Agent", userAgent);
-  // req.setHeader('Referer', url)
+  req.setHeader("Referer", 'http://www.netbian.com/');
   req.setHeader("Cookie", cookie);
   req.setHeader("Host", host);
+  // req.setHeader("If-Modified-Since", 'Thu, 08 Jul 2021 14:09:02 GMT');
+  // req.setHeader("If-None-Match", "60e706fe-1e8fd");
   // console.log(req)
   req.on("error", () => {
     isRetry = true;
@@ -43,9 +50,33 @@ function imgSpider(url, dest = "", timeout = 1000 * 3 * 60, retries = 2) {
     if (isRetry && retries > 0)
       pictureDownloader(url, dest, timeout + 60 * 1000, retries - 1);
   });
+  req.on("finish", () => {
+    console.log(dest);
+    imgToWebp(dest)
+  });
   req.end();
 }
-imgSpider(
-  "http://img.netbian.com/file/2021/0708/small328c217f576e240194847ad9c56e73741625753341.jpg"
-);
+
+/**
+ * @description : 
+ * @param        {string} dest图片路径
+ * @return       {undefined}
+ */
+imgToWebp=(dest)=>{
+  const result = webp.cwebp(
+    dest,
+    dest + ".webp",
+    "-q 80",
+    (logging = "-v")
+  );
+  result.then((response) => {
+    console.log(response);
+  });
+}
+
+
+exports=imgSpider
+// getCookie(
+//   "http://img.netbian.com/file/2021/0708/small328c217f576e240194847ad9c56e73741625753341.jpg"
+// );
 // request('http://img.netbian.com/file/2021/0708/small328c217f576e240194847ad9c56e73741625753341.jpg').pipe(fs.createWriteStream('doodle.jpg'))
