c0c4fff55b91af2601dee51bf8d29f6af17fa5b9
helishou <479525390@qq.com>
2021-07-15 16:06:11 +0800

    wybzd

--- util/liuxTest.js
+++ util/liuxTest.js
@@ -9,16 +9,15 @@
  */
 const fs = require("fs");
 const imgSpider = require("./imgSpider");
-src='/www/wwwroot/blog/cloudDisk/'
-tempUrl='smallf27b49517d6436c5bdc301fd711631e41625305469.jpg'
+src='/www/wwwroot/blog/cloudDisk'
+tempUrl='001698fa0fe48a4dbc96b36db6c2f53d.jpg'
 url=''
 fs.access(src + tempUrl, fs.constants.F_OK, (err) => {
     if (err) {
-      console.log("不存在路径", src + tempUrl);
+      console.log("不存在路�?, src + tempUrl);
       let newWebp =
         "https://www.wangxinyang.xyz/cloudDisk/" + tempUrl + ".webp";
-      imgSpider(url, src); //服务器的保存目录I是大写
-      if (tempUrl.indexOf("small") != -1) {
+      imgSpider(url, src); //服务器的保存目录I是大�?      if (tempUrl.indexOf("small") != -1) {
         //说明可以放大
         let newImgUrl = tempUrl.replace("small", "");
         newImgUrl = newImgUrl.slice(0, newImgUrl.length - 14) + ".jpg";
